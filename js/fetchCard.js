420e2dd3534c1b94db84db96c300a1b950dc01d5
Rolf43 <86641205+Rolf43@users.noreply.github.com>
2025-08-04 17:32:45 -0500

    Update fetchCard.js

--- js/fetchCard.js
+++ js/fetchCard.js
@@ -12,7 +12,7 @@ async function fetchPatientCard() {
         return;
     }
 
-    const url = `http://192.168.1.4:5001/api/records/data?name=${encodeURIComponent(patientName)}`;
+    const url = `https://nax-iot-api.onrender.com/api/records/data?name=${encodeURIComponent(patientName)}`;
 
     try {
         const response = await fetch(url, {
