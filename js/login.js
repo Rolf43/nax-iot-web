7b54ca9c9992238478cc50aa2596096a49f190ab
Rolf43 <86641205+Rolf43@users.noreply.github.com>
2025-08-04 17:46:23 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -80,10 +80,9 @@ $(document).ready(function () {
                         email,
                         password
                     }),
-                    console.log(body);
                 });
 
-
+                console.log(password);
                 // Manejar la respuesta del backend
                 const data = await response.json();
 
